9c5aa87df355d27b5b5bf3818dca8f70b5d61b84
Valpoe <tuomainen.anders@gmail.com>
2024-02-27 09:32:28 +0200

    bubble chart

--- src/pages/bubble-chart/bubbleChart.tsx
+++ src/pages/bubble-chart/bubbleChart.tsx
@@ -1,81 +1,61 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from '@mui/material';
-import { UIBackground, ChartXY, AxisTickStrategies, emptyLine, emptyFill } from '@arction/lcjs';
-import Papa from 'papaparse';
+import { UIBackground, ChartXY, emptyLine, emptyFill } from '@arction/lcjs';
 import { createChart } from '../../components/ChartHelper';
-import { CountryData } from '../../utilities/definitions';
 import '../../styles/controls-container.css';
 
 const BubbleChart = () => {
   const [chart, setChart] = useState<ChartXY<UIBackground> | undefined>(undefined);
-  const [chartData, setChartData] = useState<CountryData[]>([]);
+  const [chartData, setChartData] = useState<Array<{ x: number; y: number; size: number }>>([]);
 
   useEffect(() => {
     const licenseKey = process.env.REACT_APP_LIGHTNINGCHART_LICENSE_KEY;
 
     if (licenseKey) {
-      const chart = createChart(licenseKey, 'chart-container');
-      chart.setTitle('Bubble Chart');
-      setChart(chart);
+      const fetchData = async () => {
+        const numPoints = 100;
+        const generatedData = [];
 
-      return () => {
-        if (chart) {
-          chart.dispose();
+        for (let i = 0; i < numPoints; i++) {
+          const x = Math.random() * 10;
+          const y = Math.random() * 10;
+          const size = Math.random() * 30;
+
+          generatedData.push({ x, y, size });
         }
+
+        setChartData(generatedData);
+
+        const chart = createChart(licenseKey, 'chart-container');
+        chart.setTitle('Bubble Chart');
+        setChart(chart);
+
+        return () => {
+          if (chart) {
+            chart.dispose();
+          }
+        };
       };
+
+      fetchData();
     } else {
       console.error('LightningChart license key is undefined or chart container is null.');
     }
   }, []);
 
-  //   useEffect(() => {
-  //     if (chart) {
-  //       chart
-  //         .addPointLineAreaSeries({ dataPattern: null, sizes: true })
-  //         .setStrokeStyle(emptyLine)
-  //         .setAreaFillStyle(emptyFill)
-  //         .appendSamples({
-  //           xValues: [4, 2, 6],
-  //           yValues: [5, 2, 8],
-  //           sizes: [10, 25, 5],
-  //         });
-  //     }
-  //   }, [chart]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv'
-        );
-
-        Papa.parse(await response.text(), {
-          header: true,
-          complete: (result: CountryData) => {
-            setChartData(result.data || []);
-          },
-        });
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
-    fetchData();
-    console.log();
-  }, []);
-
   useEffect(() => {
     if (chart) {
-      const series = chart.addPointLineAreaSeries();
-      series.setPointSize(10);
-      series.add(
-        chartData.map((entry) => ({
-          x: entry.icu_patients_per_million,
-          y: entry.icu_patients_per_million,
-          size: 10,
-        }))
-      );
+      chart
+        .addPointLineAreaSeries({ dataPattern: null, sizes: true })
+        .setStrokeStyle(emptyLine)
+        .setAreaFillStyle(emptyFill)
+        .appendSamples({
+          xValues: chartData.map((d) => d.x),
+          yValues: chartData.map((d) => d.y),
+          sizes: chartData.map((d) => d.size),
+        });
     }
-  });
+  }, [chart, chartData]);
 
   return (
     <Container>
